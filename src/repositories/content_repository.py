70870df99b4cdf1d72ecce5fbe5aced0408e3800
Frank <33494411+frqnk@users.noreply.github.com>
2025-11-01 10:11:09 -0300

    Revert ""
    This reverts commit bfda40252233e1443d1ccd3d846ec8b898a35173.

--- src/repositories/content_repository.py
+++
@@ -1,9 +0,0 @@
-from goose3 import Goose
-
-
-class ContentRepository:
-    def __init__(self) -> None:
-        self._goose = Goose()
-
-    def extract_clean_text(self, url: str) -> str:
-        return self._goose.extract(url).cleaned_text
